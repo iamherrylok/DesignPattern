dab8a8e24458931377ea8a0c9ecea7c66e8f532c
iamherrylok <iamherrylok@gmail.com>
2021-08-10 09:50:04 +0800

    [原型模式]

---
+++ SimpleFactory/Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume myResume = new Resume("Lok");
+            myResume.SetPersonalInfo("男", "26");
+            myResume.SetWorkExperience("2000-2001", "BM");
+            myResume.Display();
+
+            Resume resume2 = (Resume)myResume.Clone();
+            resume2.SetWorkExperience("2001-2002", "Tencent");
+            resume2.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleFactory/Prototype/Resume.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Resume : ICloneable
+    {
+        private string Name { get; set; }
+        private string Sex { get; set; }
+        private string Age { get; set; }
+        private string TimeArea { get; set; }
+        private string Company { get; set; }
+
+        public Resume(string name)
+        {
+            Name = name;
+        }
+
+        public void SetPersonalInfo(string sex,string age)
+        {
+            Sex = sex;
+            Age = age;
+        }
+
+        public void SetWorkExperience(string timeArea,string company)
+        {
+            TimeArea = timeArea;
+            Company = company;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{ Name }  {Age }  {Sex}");
+            Console.WriteLine($"{TimeArea}  :  {Company}");
+        }
+
+        public object Clone()
+        {
+            return (object)this.MemberwiseClone();
+        }
+    }
+}
