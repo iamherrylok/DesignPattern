221ab1d9479c84d1b4416bd07664248ab1f7215d
iamherrylok <iamherrylok@gmail.com>
2021-08-19 16:22:23 +0800

    [组合模式]

---
+++ SimpleFactory/Composite/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    abstract class Component
+    {
+        public string Name { get; set; }
+        public Component(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+
+
+    }
+}
---
+++ SimpleFactory/Composite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 树模型就是组合模式很好的解释
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("Root");
+            root.Add(new Composite("subRoot1"));
+            root.Add(new Composite("subRoot2"));
+
+            Composite lok = new Composite("lok");
+            lok.Add(new Leaf("lokk"));
+            lok.Add(new Leaf("lokkk"));
+
+            root.Add(lok);
+
+        }
+    }
+}
